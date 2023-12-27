ac2e382ef0c1d9d3a50b9ef7d33c30b4ffb5acf3
tomo324 <0204tomo.mc@gmail.com>
2023-12-27 20:53:51 +0900

    defined dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, PrismaModule, PrismaModule, UserModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AuthService {
-  constructor() {}
+  constructor(private prisma: PrismaService) {}
   signup() {
     return { message: 'signup' };
   }
