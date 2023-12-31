b557a5f3907a189298650fee0515e5622d16f6c5
tomo324 <0204tomo.mc@gmail.com>
2023-12-31 23:04:30 +0900

    prisma schemaを修正

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2'
+import { AuthDto } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup() {
-    return { message: 'signup' };
+  async signup(dto: AuthDto) {
+    // generate the hash of the password
+    const hash = await argon.hash(dto.password);
+    // save the new user in the db
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Email already exists');
+        }
+      }
+      throw error;
+    }
   }
 
-  signin() {
+  async signin(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
     return { message: 'signin' };
   }
 }
