82a4852a1b7928f131fea5ae77ebd6201154a27c
tomo324 <0204tomo.mc@gmail.com>
2024-03-28 17:35:56 +0900

    🧪 #17 サービスのユニットテストを作成

--- src/post/test/post.service.spec.ts
+++ src/post/test/post.service.spec.ts
@@ -1,18 +1,136 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PostService } from '../post.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('PostService', () => {
   let service: PostService;
+  let prismaService: PrismaService;
+
+  const mockPrismaService = {
+    post: {
+      createPost: jest.fn().mockResolvedValue({
+        id: 1,
+        content: 'test content',
+        authorId: 1,
+      }),
+      getManyPosts: jest.fn().mockResolvedValue([
+        {
+          id: 1,
+          content: 'test content',
+          authorId: 1,
+        },
+        {
+          id: 2,
+          content: 'test content',
+          authorId: 1,
+        },
+      ]),
+      getOnePost: jest.fn().mockResolvedValue({
+        id: 1,
+        content: 'test content',
+        authorId: 1,
+      }),
+      getManyPostsByUserId: jest.fn().mockResolvedValue([
+        {
+          id: 1,
+          content: 'test content',
+          authorId: 1,
+        },
+        {
+          id: 2,
+          content: 'test content',
+          authorId: 1,
+        },
+      ]),
+      deletePost: jest.fn(),
+    },
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostService],
+      providers: [
+        PostService,
+        { provide: PrismaService, useValue: mockPrismaService },
+      ],
     }).compile();
 
     service = module.get<PostService>(PostService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
-  it('should be defined', () => {
+  it('serviceが定義されていること', () => {
     expect(service).toBeDefined();
+  });
+  it('投稿を作成でき、作成した投稿を返すこと', async () => {
+    const userId = 1;
+    const createPostDto = {
+      content: 'test content',
+    };
+    const result = await service.createPost(userId, createPostDto);
+    expect(prismaService.post.create).toHaveBeenCalledWith({
+      data: {
+        content: createPostDto.content,
+        authorId: userId,
+      },
+    });
+    expect(result).toEqual({
+      id: 1,
+      content: 'test content',
+      authorId: 1,
+    });
+  });
+  it('投稿を複数取得でき、取得した投稿を返すこと', async () => {
+    const result = await service.getManyPosts();
+    expect(prismaService.post.findMany).toHaveBeenCalled();
+    expect(result).toEqual([
+      {
+        id: 1,
+        content: 'test content',
+        authorId: 1,
+      },
+      {
+        id: 2,
+        content: 'test content',
+        authorId: 1,
+      },
+    ]);
+  });
+  it('特定の投稿を取得でき、取得した投稿を返すこと', async () => {
+    const postId = 1;
+    const result = await service.getOnePost(postId);
+    expect(prismaService.post.findUnique).toHaveBeenCalledWith({
+      where: { id: postId },
+    });
+    expect(result).toEqual({
+      id: 1,
+      content: 'test content',
+      authorId: 1,
+    });
+  });
+  it('特定のユーザーの投稿を複数取得でき、取得した投稿を返すこと', async () => {
+    const userId = 1;
+    const result = await service.getManyPostsByUserId(userId);
+    expect(prismaService.post.findMany).toHaveBeenCalledWith({
+      where: { authorId: userId },
+    });
+    expect(result).toEqual([
+      {
+        id: 1,
+        content: 'test content',
+        authorId: 1,
+      },
+      {
+        id: 2,
+        content: 'test content',
+        authorId: 1,
+      },
+    ]);
+  });
+  it('投稿を削除できること', async () => {
+    const postId = 1;
+    await service.deletePost(postId);
+    expect(prismaService.post.delete).toHaveBeenCalledWith({
+      where: { id: postId },
+    });
   });
 });
