9d69550b973135ef62ed5667efc75116c38d5a62
tomo324 <0204tomo.mc@gmail.com>
2024-03-31 15:22:43 +0900

    🐛 #17 e2eテストでユーザー削除の前に投稿の削除を行う

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -15,6 +15,6 @@ export class PrismaService extends PrismaClient {
   }
 
   cleanDb() {
-    return this.$transaction([this.user.deleteMany()]);
+    return this.$transaction([this.post.deleteMany(), this.user.deleteMany()]);
   }
 }
